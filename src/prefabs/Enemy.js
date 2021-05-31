72b163c2fe30b85ee3928e8dc3165cb1b8d091b2
Zhendong Jiang <zjiang40@ucsc.edu>
2021-05-30 22:41:41 -0700

    initial

--- src/prefabs/Enemy.js
+++ src/prefabs/Enemy.js
@@ -13,22 +13,14 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
         super(scene, game.config.width + 32, Phaser.Math.Between(128/2, game.config.height - 128/2), 'whales');
         scene.add.existing(this);           // add object to the existing scene
         scene.physics.add.existing(this);   // add to physics system
-        this.setSize(128, 64);
-        this.setOffset(32, -8);
-        this.setVelocityX(velocity);
-        this.setImmovable();
-        this.newWhale = true;
+        // this.setSize(128, 64);
+        // this.setOffset(32, -8);
+        // this.setVelocityX(velocity);
+        // this.setImmovable();
+        // this.newWhale = true;
     }
 
     update() {
-        if(this.newWhale && this.x < game.config.width/8) {
-            this.newWhale = false;
-            this.scene.addWhale(this.velocity);
-        }
-
-        // destroy whale if it reaches the left edge of the screen
-        if(this.x < -this.width) {
-            this.destroy();
-        }
+        
     }
 }
