58413a444048dc386dce03c8c8741ec9b7058164
Nathan Pon <nathanpon98@gmail.com>
2021-06-02 22:28:58 -0700

    player.bulletGroup for prefab collision

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -33,10 +33,10 @@ class Level1 extends Phaser.Scene {
 
         //Crosshair and UI
         // this.p1 = this.add.sprite(0, 0, 'crosshair');
-        this.add.rectangle(this.cameras.x+16,borderUISize + borderPadding, game.config.width/4, borderUISize * 2,  0x00FF00).setOrigin(0,0.7); 
-        this.healthText = this.add.text(this.cameras.x + 16, 16 , `Health: ${this.playerHP}`, { fontSize: '16px', fill: '#000' }).setScrollFactor(0);
+      //  this.add.rectangle(16,borderUISize + borderPadding, game.config.width/4, borderUISize * 2,  0x00FF00).setScrollFactor(0); 
+        this.healthText = this.add.text(16, 16 ,`Health: ${this.playerHP}`, { fontSize: '16px', fill: '#000' }).setScrollFactor(0);
         
-        //this.add.rectangle(0,borderUISize + borderPadding, game.config.width/4, borderUISize * 2, 0xFEEEBC).setOrigin(-4,0.7);  
+        //this.add.rectangle(0,borderUISize + borderPadding, game.config.width/4, borderUISize * 2, 0x00FF00).setScrollFactor(0);  
         this.ammoText = this.add.text(16, 32, `Ammo: ${this.ammoCount}`, { fontSize: '16px', fill: '#000' }).setScrollFactor(0);
         
 
@@ -50,6 +50,9 @@ class Level1 extends Phaser.Scene {
 
         this.spawnEnemies1();
 
+      //  const enemySpawn = map.findObjects("Enemy1", obj => obj.name === "Enemy Spawn1");
+
+     
         this.physics.world.gravity.y = 2000;
         this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
         this.physics.add.collider(player, platformLayer);
@@ -67,45 +70,25 @@ class Level1 extends Phaser.Scene {
         //     this.loopCall();  
         // }) 
            
-        this.shooting();
         
     }
 
-    shooting(){
-        // Fire projectile on click
-        this.input.on('pointerdown', (pointer) =>{ 
-            // Position Projectile to spawn from player's position
-            // this.bullet = new projectile (this, player.x, player.y, 'projectile');
-            this.sfx = this.sound.add('gunshot', {
-                mute: false,
-                volume: 0.1,
-                rate: 1,
-                loop: false 
-            });
-            this.sfx.play();
-            // this.bullet.body.velocity.x =this.p1.x-300; //projectile physics
-            // this.bullet.body.velocity.y =this.p1.y-300; 
-            
-            this.ammoCount -= 1
-            this.ammoText.text = `Ammo: ${this.ammoCount}`;   
-        })
-    }
     spawnEnemies1() {
-        // const enemySpawn = this.map.getObjectLayer('Enemy1');
+         //const enemySpawn = this.map.getObjectLayer('Enemy1');
         this.enemyGroup = this.add.group({
-            // runChildUpdate: true
+          //   runChildUpdate: true
         });
         // enemySpawn.objects.forEach(objects => {
-        //     this.addEnemy(objects);
-        // })
-        this.addEnemy();
+             this.addEnemy();
+        // })   
     }
 
     addEnemy() {
-        // const enemySpawn = this.map.findObject("Object", obj => obj.name === "Enemy Spawn");
-        let movementSpeed = Phaser.Math.Between(0, 50);
-        enemy1 = new Enemy(this, -100, movementSpeed, 400);
-        this.enemyGroup.add(enemy1);
+        
+      //const enemySpawn = this.map.findObjects("Enemy1", obj => obj.name === "Enemy Spawn1");
+      let movementSpeed = Phaser.Math.Between(0, 50);
+      enemy1 = new Enemy(this, -100, movementSpeed, 400);
+      this.enemyGroup.add(enemy1);
     }
     // spawnBoss(){
     //     this.boss1 = new Enemy(this, Phaser.Math.Between(game.config.width, game.config.width/2), Phaser.Math.Between(0,  game.config.height)).setOrigin(0.5,1);
@@ -125,21 +108,20 @@ class Level1 extends Phaser.Scene {
         //     this.p1.x = pointer.x;
         //     this.p1.y = pointer.y;
         //     })
-
-            this.physics.overlap(player, this.enemyGroup, this.takeDamage, null, this)
-            this.physics.overlap(this.enemyGroup, this.bullet, this.hitEnemy, null, this)
-            
             player.update();
+            
+            this.physics.add.overlap(this.enemyGroup, player, this.takeDamage, null, this)
+            this.physics.overlap(this.enemyGroup, player.bulletGroup, this.hitEnemy, null, this)
         
             if(Phaser.Input.Keyboard.JustDown(this.swap)) {
             this.scene.start("level3Scene");
         }
     }
 
-    takeDamage(player, sprite){
+    takeDamage(sprite, player){
         console.log('hit');
-        this.playerHP -=1
-        this.healthText.text = `Health: ${this.playerHP}`
+        this.playerHP -=1;
+        this.healthText.text = `Health: ${this.playerHP}`;  
         
         this.cameras.main.shake(250, 0.0075);
         if( this.playerHP <=0)
@@ -148,10 +130,10 @@ class Level1 extends Phaser.Scene {
         }
     }
 
-    hitEnemy(sprite, bullet) {
+    hitEnemy(sprite, bulletGroup) {
         console.log('hit');
         sprite.hit();
-        bullet.destroy();
+        bulletGroup.destroy();
         this.collide = this.sound.add('monsterHit', {
         mute: false,
         volume: 1,
